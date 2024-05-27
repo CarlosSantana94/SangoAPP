197f04f0fc4dfbff72333c9aa8f49378036b4d8e
Carlos Santana <cgsp94@gmail.com>
2024-05-27 02:40:11 -0600

    Update Android Working google

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,31 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bucapps.sango',
   appName: 'SANGO',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  },
+  plugins: {
+    GoogleAuth: {
+      androidClientId: '726792798295-vbgcc63j11lu3k81fc588ft4duguat34.apps.googleusercontent.com',
+      serverClientId: "726792798295-vbgcc63j11lu3k81fc588ft4duguat34.apps.googleusercontent.com",
+      scopes: [
+        'profile',
+        'email'
+      ],
+      forceCodeForRefreshToken: true
+    }
+  }
+
 };
 
 export default config;
