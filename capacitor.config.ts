aac537217ba4a38d903f6ed6c7b65ba93508ab8a
Carlos Santana <cgsp94@gmail.com>
2024-05-27 02:16:40 -0600

    Update IOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SangoPRuebaAppflow',
+  appId: 'com.bucapps.sango',
+  appName: 'SANGO',
   webDir: 'www'
 };
 
