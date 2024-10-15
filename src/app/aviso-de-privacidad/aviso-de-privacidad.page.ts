7cad60b49edec36418566eec427b35bc2980e36e
Carlos SP <cgsp94@gmail.com>
2024-10-15 03:01:52 -0600

    Multiples Cambios visuales y de logica

---
+++ src/app/aviso-de-privacidad/aviso-de-privacidad.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aviso-de-privacidad',
+  templateUrl: './aviso-de-privacidad.page.html',
+  styleUrls: ['./aviso-de-privacidad.page.scss'],
+})
+export class AvisoDePrivacidadPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
