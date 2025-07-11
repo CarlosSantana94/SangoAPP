a848c4620699ca8fb83d128b030726b36847411a
Carlos SP <cgsp94@gmail.com>
2025-07-11 12:53:23 -0600

    V2.9

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import Capacitor
 import FirebaseCore
 import FirebaseMessaging
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
+        var handled: Bool
+
+            handled = GIDSignIn.sharedInstance.handle(url)
+            if handled {
+              return true
+            }
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
